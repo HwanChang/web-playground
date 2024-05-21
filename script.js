aeaf41338fd33e855b1dc8364ae4d0accad7c302
HwanChang <hwanchang.dev@gmail.com>
2024-05-22 01:26:45 +0900

    ✨ 결과물 리스트 페이지 생성

---
+++ script.js
@@ -0,0 +1,20 @@
+var pages = [
+    { title: 'Login', description: '로그인 페이지', directory: 'LoginPage' },
+]
+
+var container = document.getElementById('container');
+var cards = pages.map(page => {
+    return `<div class="card">
+        <div class="card-image">
+            <iframe src="${page.directory}/index.html"></iframe>
+        </div>
+        <a href="${page.directory}/index.html">
+            <div class="card-content">
+                <h3 class="card-title">${page.title}</h3>
+                <p class="card-description">${page.description}</p>
+            </div>
+        </a>
+    </div>`
+})
+
+container.innerHTML = cards.join('');
